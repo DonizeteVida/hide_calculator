0ad6dd065734f860d4e8862f47cb386f34a384bb
DonizeteVida <donizetevida1998@outlook.com>
2020-06-21 12:03:10 -0300

    Novos tipos de build no vscode

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
 {
     for (int i = 0; i < argc; i++)
     {
-        printf("arg n %d is %s", i, argv[i]);
+        printf("arg n %d is %s\n", i, argv[i]);
     }
     somatorio1(1, 1);
     return 0;
