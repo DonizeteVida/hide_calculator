c86462e5317d075b16d238477fa864d7611652dd
Donizete Vida <donizetevida1998@outlook.com>
2020-06-25 01:17:25 -0300

    Trying to make something with union keyword fails

--- logica_proposicional/logica_proposicional.c
+++ logica_proposicional/logica_proposicional.c
@@ -2,42 +2,53 @@
 #include <stdlib.h>
 #include "logica_proposicional.h"
 
+typedef int (*DoubleCheck)(int, int);
+typedef int (*SingleCheck)(int);
+
+void _processDouble(DoubleCheck doubleCheck)
+{
+
+}
+
+void _processSingle(SingleCheck singleCheck,const char*const who)
+{
+}
+
 void showLogicaProposicionalSubmenu()
 {
     int opc = 0;
-
     do
     {
-        system("cls");
+        system("clear");
         printf("Logica proposicional\n1. ~p\n2. ~q\n3. p ∧ q\n4. p ∨ q\n5. p <> q\n6. p -> q\n7. p <-> q\n8. Voltar ao menu anterior.\n\n");
         scanf("%d", &opc);
 
         switch (opc)
         {
         case 1:
+            _processSingle(&not, "p");
             break;
         case 2:
+            _processSingle(&not, "q");
             break;
         case 3:
+            _processDouble(&and);
             break;
         case 4:
+            _processDouble(& or);
             break;
         case 5:
+            _processDouble(&xor);
             break;
         case 6:
+            _processDouble(&ifJust);
             break;
         case 7:
-            break;
-        case 8:
-            break;
-        default:
+            _processDouble(&ifJustIf);
             break;
         }
     } while (opc != 8);
 }
-
-typedef int (*DoubleCheck)(int, int);
-typedef int (*SingleCheck)(int);
 
 int not(int value)
 {
