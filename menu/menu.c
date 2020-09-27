a11db21516b0b755a05c3eeb69a0aaea6f6826e0
Johnny Lorran <johnnylsrdarocha@gmail.com>
2020-09-27 17:45:06 -0300

    Removido código velho

--- menu/menu.c
+++ menu/menu.c
@@ -1,37 +1,7 @@
-#include <sys/ioctl.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 #include "menu.h"
-
-typedef struct winsize Size;
 
 void printProgramName(const char *const name)
 {
-    Size size;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-    for (int i = 0; i < size.ws_col; i++)
-    {
-        printf("%%");
-    }
-    printf("\n\n");
-    //***************************************
-    int quantity = strlen(name);
-    int diference = size.ws_col - quantity;
-    for (int i = 0; i < diference / 2; i++)
-    {
-        printf(" ");
-    }
     printf("%s", name);
-    for (int i = 0; i < diference / 2; i++)
-    {
-        printf(" ");
-    }
-    printf("\n\n");
-    //***************************************
-    for (int i = 0; i < size.ws_col; i++)
-    {
-        printf("%%");
-    }
-    printf("\n\n");
 }
